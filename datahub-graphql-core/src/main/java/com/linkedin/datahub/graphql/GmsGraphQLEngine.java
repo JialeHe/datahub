3807567206c3d4315877861abf2c7e91031e0844
John Joyce <john@Mac.lan>
2026-04-03 14:01:34 -0700

    Adding tests

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/GmsGraphQLEngine.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/GmsGraphQLEngine.java
@@ -436,6 +436,7 @@ public class GmsGraphQLEngine {
   private ConnectionService connectionService;
   private AssertionService assertionService;
   private final DocumentService documentService;
+  private final com.linkedin.metadata.service.docimport.DocumentImportService documentImportService;
   private final EntityVersioningService entityVersioningService;
   private final ApplicationService applicationService;
   private final PageTemplateService pageTemplateService;
@@ -585,6 +586,7 @@ public class GmsGraphQLEngine {
     this.connectionService = args.connectionService;
     this.assertionService = args.assertionService;
     this.documentService = args.documentService;
+    this.documentImportService = args.documentImportService;
     this.entityVersioningService = args.entityVersioningService;
 
     this.businessAttributeService = args.businessAttributeService;
@@ -3095,7 +3097,8 @@ public class GmsGraphQLEngine {
             this.graphClient,
             entityRegistry,
             this.timelineService,
-            this.groupService)
+            this.groupService,
+            this.documentImportService)
         .configureResolvers(builder);
   }
 
